d6438938d9a4826120d98327b1dd47dbe08b4bd5
wdyzmx <wdyzmx@wdyzmxdeMacBook-Pro.local>
2018-06-29 18:08:34 +0800

    MenuListModel

---
+++ CustomTabBarMenu/model/MenuListModel.h
@@ -0,0 +1,13 @@
+//
+//  MenuListModel.h
+//  CustomTabBarMenu
+//
+//  Created by wdyzmx on 2018/6/29.
+//  Copyright © 2018年 wdyzmx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MenuListModel : NSObject
+
+@end
---
+++ CustomTabBarMenu/model/MenuListModel.m
@@ -0,0 +1,13 @@
+//
+//  MenuListModel.m
+//  CustomTabBarMenu
+//
+//  Created by wdyzmx on 2018/6/29.
+//  Copyright © 2018年 wdyzmx. All rights reserved.
+//
+
+#import "MenuListModel.h"
+
+@implementation MenuListModel
+
+@end
